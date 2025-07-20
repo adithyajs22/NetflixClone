07492c9d0d4bde864d3309ad7b4f02c7725ee863
adithyajs <adithyajs2210@gmail.com>
2025-07-20 17:40:34 +0530

    search items

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,62 @@ import './App.css'
 import Banner from './Components/Banner/Banner.jsx';
 import RowPost from './Components/RowPost/RowPost.jsx';
 import { action,comedyMovies,documentaries,horrorMovies,originals, romanceMovies } from '../urls.js';
+import { useState } from 'react';
+import YouTube from 'react-youtube';
+import { API_KEY } from './Constants/Constants.js';
+import axios from 'axios';
 
 
 function App() {
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const[urlId,setUrlId] = useState('')
+   const[movies,setMovies] = useState('')
+   const opts = {
+      height: '450',
+      width: '100%',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
+    const handleMovies=(id)=>{
+      axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`)
+       .then((response)=>{
+      console.log(response.data)
+     if(response.data.results.length!== 0){
+      setUrlId(response.data.results[0])}
+      setMovies(id)
+    })
+    }
   return (
-    <div className='app'>
-      <NavBar/>
-      <Banner/>
-      <RowPost url={originals}name='Netflix Originals'/>
-      <RowPost url={action}name='Action' isSmall isSmallTag/>
-      <RowPost url={comedyMovies}name='Comedy ' isSmall isSmallTag/>
-      <RowPost url={horrorMovies}name='Horror' isSmall isSmallTag/>
-        <RowPost url={romanceMovies}name='Romance' isSmall isSmallTag/>
-      <RowPost url={documentaries}name='Documentaries' isSmall isSmallTag/>
+ <div className='app'>
+  <NavBar setFilteredMovies={setFilteredMovies} />
+  <Banner />
+  {filteredMovies.length > 0 && (
+    <div className="search-results">
+      <h2>Search Results</h2>
+      <div className="row-posts">
+        {filteredMovies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleMovies(movie.id)}
+            className="search-poster"
+            src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+            alt={movie.title || movie.name}
+          />
+        ))}
+      </div>
+      {urlId && <YouTube videoId={urlId.key} opts={opts} />}
     </div>
+  )}  
+  <RowPost url={originals} name='Netflix Originals' />
+  <RowPost url={action} name='Action' isSmall isSmallTag />
+  <RowPost url={comedyMovies} name='Comedy' isSmall isSmallTag />
+  <RowPost url={horrorMovies} name='Horror' isSmall isSmallTag />
+  <RowPost url={romanceMovies} name='Romance' isSmall isSmallTag />
+  <RowPost url={documentaries} name='Documentaries' isSmall isSmallTag />
+</div>
+
   );
 }
 
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -6,31 +6,31 @@ import { BiSolidBell } from "react-icons/bi";
 import axios from 'axios';
 import { API_KEY } from '../../Constants/Constants';
 
-function NavBar() {
-  const [searchItems,setSearchItems]=useState('')
-   const [filteredMovies,setFilteredMovies]=useState([])
-   
-   useEffect(()=>{
-  const handleSearch=()=>{
-    axios.get(`/search/multi?api_key=${API_KEY}&language=en-US&query=${searchItems}`)
-  .then((response)=>{
-      const movies = response.data.results;
+function NavBar({setFilteredMovies}) {
+  const [searchItems,setSearchItems] = useState('')
+
+useEffect(() => {
+  const handleSearch =()=>{  axios.get(
+        `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${searchItems}`
+      )
+      .then((response)=>{
+
+      const movies = response.data.results || [];
+      const filtered = movies.filter((movie) =>
+        (movie.title || movie.name || "").toLowerCase().includes(searchItems.toLowerCase())
+      );
+
+      setFilteredMovies(filtered);
       
-      console.log(movies)
-      const filtered = movies.filter((movie)=>
-        movie.title.toLowerCase().includes(searchItems.toLowerCase())
-     
-  );
-  setFilteredMovies(filtered);
-   console.log("Search term:", searchItems);
-      
-    if (searchItems.trim()) {
-    handleSearch();
-  } else {
-    setFilteredMovies([]);
+    });
   }
-  })}
-  },[searchItems])
+      if (searchItems.trim()) {
+      handleSearch();
+    } else {
+      setFilteredMovies([]); 
+    }
+  }, [searchItems]);
+
   return (
     <div className='navbar'>
         <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="logo" />
@@ -38,6 +38,9 @@ function NavBar() {
         <IoSearch className='search-icon'/>
         <input  type='text'className='search'placeholder='Search for movies...' value={searchItems} onChange={(e)=>setSearchItems(e.target.value)}/>
         <BiSolidBell className='bell' />
+        
+
+          
     </div>
     
   )
